68f99fafcc60ac8d45437fe4a10b392892326759
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-07-14 14:02:28 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 basic.forever(function () {
     let 方位 = 0
-    if (方位 > 270 || 方位 < 90) {
+    if (方位 > 377.5 || 方位 < 22.5) {
         basic.showArrow(ArrowNames.North)
-    } else {
+        music.playMelody("C5 B A G F E D C ", 120)
+    } else if (方位 < 67.5) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else if (方位 < 112.5) {
+        basic.showArrow(ArrowNames.West)
+    } else if (方位 < 157.5) {
+    	
+    } else if (方位 < 202.5) {
         basic.showArrow(ArrowNames.South)
+    } else if (方位 < 247.5) {
+    	
+    } else if (方位 < 292.5) {
+        basic.showArrow(ArrowNames.East)
+    } else {
+        basic.showArrow(ArrowNames.NorthEast)
     }
 })
